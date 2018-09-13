d2fbef1addd0796f39d5b5cb46c6d464acaa45fd
xupeng <1039731508@qq.com>
2018-09-13 18:22:45 +0800

    新增品牌增删改查

--- fenfengou-sellergoods-interface/src/main/java/com/fenfengou/sellergoods/service/BrandService.java
+++ fenfengou-sellergoods-interface/src/main/java/com/fenfengou/sellergoods/service/BrandService.java
@@ -2,6 +2,7 @@ package com.fenfengou.sellergoods.service;
 
 import java.util.List;
 
+import com.fenfengou.entity.PageResult;
 import com.fenfengou.pojo.TbBrand;
 /**
  * 品牌服务层口
@@ -9,5 +10,11 @@ import com.fenfengou.pojo.TbBrand;
  *
  */
 public interface BrandService {
-	List<TbBrand> findAll();
+	public List<TbBrand> findAll();
+	public PageResult findPage(int pageNum,int pageSize);
+	public void add(TbBrand brand);
+	public TbBrand findOne(Long id);
+	public void update(TbBrand brand);
+	public void delete(Long[] ids);
+	public PageResult findPage(TbBrand brand,int pageNum,int pageSeze);
 }
